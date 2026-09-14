40aac384de56d8662c503fc64121a368973436d9
mlange-42 <martin_lange_@gmx.net>
2026-09-14 13:11:29 +0200

    Use column-major order for trees grid

--- res/tree_grid_test.go
+++ res/tree_grid_test.go
@@ -19,7 +19,7 @@ func TestTreeGrid(t *testing.T) {
 	w := ecs.NewWorld()
 	e := w.NewEntity()
 
-	g.Set(5, 0, e)
+	g.Set(0, 5, e)
 	assert.False(t, g.trees[5].IsZero())
 
 	assert.Equal(t, e, g.Get(5, 0))
