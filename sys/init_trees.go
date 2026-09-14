cef37f2e4026452f05acc24510406e7d85785f7f
mlange-42 <martin_lange_@gmx.net>
2026-09-14 10:06:08 +0200

    fix linter warnings

--- sys/init_trees.go
+++ sys/init_trees.go
@@ -33,14 +33,14 @@ func (s *InitTrees) Initialize(world *ecs.World) {
 	builder := ecs.NewMap1[comp.Position](world)
 
 	cnt := 0
-	builder.NewBatchFn(len(cells), func(e ecs.Entity, p *comp.Position) {
+	builder.NewBatchFn(len(cells), func(_ ecs.Entity, p *comp.Position) {
 		*p = cells[cnt]
 		cnt++
 	})
 }
 
 // Update the system.
-func (s *InitTrees) Update(w *ecs.World) {}
+func (s *InitTrees) Update(_ *ecs.World) {}
 
 // Finalize the system.
-func (s *InitTrees) Finalize(w *ecs.World) {}
+func (s *InitTrees) Finalize(_ *ecs.World) {}
