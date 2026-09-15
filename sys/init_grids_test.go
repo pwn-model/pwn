b028b789ad1b97006c0da6320dad7048d0014099
mlange-42 <martin_lange_@gmx.net>
2026-09-15 14:12:44 +0200

    add test

---
+++ sys/init_grids_test.go
@@ -0,0 +1,28 @@
+package sys
+
+import (
+	"testing"
+
+	"github.com/mlange-42/ark-tools/app"
+	"github.com/mlange-42/ark/ecs"
+	"github.com/pwn-model/pwn/res"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitGrids(t *testing.T) {
+	app := app.New()
+	ws := res.WorldSize{Width: 25, Height: 12, Resolution: 10}
+	ecs.AddResource(app.World, &ws)
+
+	s := InitGrids{}
+	s.Initialize(app.World)
+
+	trees := ecs.GetResource[res.EntityGrid](app.World)
+	assert.Equal(t, ws.Width, trees.Width())
+	assert.Equal(t, ws.Height, trees.Height())
+
+	space := ecs.GetResource[res.SpaceGrid](app.World)
+	grid := res.EntityGrid(*space)
+	assert.Equal(t, 3, grid.Width())  // ceil(25/10)
+	assert.Equal(t, 2, grid.Height()) // ceil(12/10)
+}
