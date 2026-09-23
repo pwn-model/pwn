7e1863d4b9d2a6bf06ee91f5bae00813d217b85e
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2026-09-23 22:36:27 +0200

    Nematode infection from feeding beetles (#26)

--- benchmark/compare/run_model.go
+++ benchmark/compare/run_model.go
@@ -60,9 +60,9 @@ func setup(width, height, ticks int) *app.App {
 	app.AddSystem(&sys.DiseaseCourse{
 		TicksToDamage: 8,
 	})
-	app.AddSystem(&sys.RandomInfection{
+	app.AddSystem(&sys.RandomRelease{
 		TickOfInfection: 0,
-		NumTrees:        100,
+		NumTrees:        10,
 		CellX:           10,
 		CellY:           10,
 	})
@@ -73,7 +73,7 @@ func setup(width, height, ticks int) *app.App {
 	})
 	app.AddSystem(&sys.BeetleEmergence{
 		TickOfYear:     19,
-		BeetlesPerTree: 10,
+		BeetlesPerTree: 5,
 		LifeExpectancy: 5,
 	})
 	app.AddSystem(&sys.TreeAttraction{
@@ -92,9 +92,9 @@ func setup(width, height, ticks int) *app.App {
 	})
 	app.AddSystem(&sys.BeetleMovement{
 		StepsPerTick:          7,
-		DurationFeeding:       12,
+		DurationFeeding:       6,
 		DurationEggLaying:     6,
-		LeaveTreeProbability:  0.5,
+		LeaveTreeProbability:  0.1,
 		RandomWalkProbability: 0.5,
 	})
 	app.AddSystem(&sys.BeetleMortality{})
@@ -105,6 +105,9 @@ func setup(width, height, ticks int) *app.App {
 		KernelRadius:       300,
 		BeetlesPerTree:     2.2,
 		TreesPerBeetle:     1,
+	})
+	app.AddSystem(&sys.NematodeInfection{
+		InfectionProbability: 0.01,
 	})
 
 	// Stop criterion
---
+++ sys/nematode_infection.go
@@ -0,0 +1,72 @@
+package sys
+
+import (
+	"math"
+	"math/rand/v2"
+
+	"github.com/mlange-42/ark-tools/resource"
+	"github.com/mlange-42/ark/ecs"
+	"github.com/pwn-model/pwn/comp"
+	"github.com/pwn-model/pwn/config"
+	"github.com/pwn-model/pwn/res"
+)
+
+func init() {
+	config.Register[NematodeInfection]()
+}
+
+// NematodeInfection from feeding, infected beetles.
+type NematodeInfection struct {
+	InfectionProbability float64 `yaml:"infection_probability"`
+
+	timeRes    ecs.Resource[res.Time]
+	feedingRes ecs.Resource[res.FeedingInfectedBeetles]
+	randRes    ecs.Resource[resource.Rand]
+
+	filter *ecs.Filter1[comp.Position]
+	mapper *ecs.Map1[comp.Infected]
+
+	toInfect []ecs.Entity
+}
+
+// Initialize the system.
+func (s *NematodeInfection) Initialize(world *ecs.World) {
+	s.timeRes = s.timeRes.New(world)
+	s.feedingRes = s.feedingRes.New(world)
+	s.randRes = s.randRes.New(world)
+	s.filter = s.filter.New(world).Without(ecs.C[comp.Damaged](), ecs.C[comp.Infected]())
+	s.mapper = s.mapper.New(world)
+}
+
+// Update the system.
+func (s *NematodeInfection) Update(_ *ecs.World) {
+	tick := s.timeRes.Get().Tick
+	feeding := s.feedingRes.Get().Grid
+	rng := rand.New(s.randRes.Get())
+	nonInfProb := 1.0 - s.InfectionProbability
+
+	q := s.filter.Query()
+	for q.NextTable() {
+		entities := q.Entities()
+		positions := q.GetColumns()
+		for i := range entities {
+			pos := &positions[i]
+			f := feeding.Get(pos.X, pos.Y)
+			if f == 0 {
+				continue
+			}
+			p := 1.0 - math.Pow(nonInfProb, float64(f))
+			if rng.Float64() < p {
+				s.toInfect = append(s.toInfect, entities[i])
+			}
+		}
+	}
+
+	for _, e := range s.toInfect {
+		s.mapper.Add(e, &comp.Infected{InfectionTick: tick})
+	}
+	s.toInfect = s.toInfect[:0]
+}
+
+// Finalize the system.
+func (s *NematodeInfection) Finalize(_ *ecs.World) {}
